06f3b7a88738b8b16afa628c5cda7dacfdfa10ac
Beau <beau28713@gmail.com>
2023-07-03 21:45:04 -0400

    added Docstrings

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+"""Graphical Calculator App
+
+App that uses the kivy Graphical framework to calculate
+basic mathmatical functions such as:
+
+Addition
+Subtraction
+Multiplication
+Division
+Powers
+"""
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
-
 class Calculator(BoxLayout):
+    """GUI setup and build
+
+    Used to set up the boxlayout grid, text inputs and buttons.
+      
+
+    Args:
+        BoxLayout (Container): layout type
+    """
+
     def __init__(self, **kwargs):
         super(Calculator, self).__init__(**kwargs)
         self.orientation = "vertical"
@@ -38,7 +57,16 @@ class Calculator(BoxLayout):
         )
         self.add_widget(self.equal_button)
 
-    def on_button_press(self, instance):
+    def on_button_press(self, instance)-> None:
+        """Button being pressed
+
+        Captures the instance of the button being pressed at that time.
+        Pulls the text from the current button. If the text is 'c' then the output is
+        cleared. Else it is stored in a varible to use in the solution evaluation. 
+
+        Args:
+            instance (buttton pressed): instance of the button being pressed
+        """
         button_pressed = instance.text
 
         if button_pressed == "C":
@@ -48,7 +76,15 @@ class Calculator(BoxLayout):
 
             self.solution_output.text = current_text
 
-    def solution(self, instance):
+    def solution(self, instance)-> None:
+        """Evaluation and Exception handeling
+
+        Evaluate the user input and catches any error that might be generated. 
+        Then display the solution or Exception error to the text input box.
+
+        Args:
+            instance (Button): Instance of the equal button being pressed
+        """
         try:
             self.output = eval(self.solution_output.text)
             self.solution_output.text = str(self.output)
@@ -70,4 +106,4 @@ class MY_App(App):
 
 
 if __name__ == "__main__":
-    MY_App().run()
+   MY_App().run()
